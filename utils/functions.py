41a96baa55b83b7e7939fe4f0ab01135326bdfa5
Sam Gathercole <sam@nextail.co>
2019-04-23 12:44:04 +0200

    Clean reviews text of training data

---
+++ utils/functions.py
@@ -0,0 +1,57 @@
+import pandas as pd
+import csv
+from bs4 import BeautifulSoup
+import re
+import nltk
+try:
+    nltk.data.find('corpora/stopwords.zip')
+except LookupError:
+    nltk.download('stopwords')
+from nltk.corpus import stopwords
+
+HEADER_ROW = 0
+DELIMITER = '\t'
+
+def read_dataset(name):
+    path = 'data/' + name
+    return pd.read_csv(
+        path,
+        header=HEADER_ROW,
+        delimiter=DELIMITER,
+        quoting=csv.QUOTE_NONE
+    )
+
+def process_reviews(reviews):
+    all_reviews = reviews['review']
+    clean_reviews = []
+    for i, review in enumerate(all_reviews):
+        if((i+1) % 1000 == 0 ):
+            print("Review %d of %d\n" % (i+1, len(all_reviews)))
+        clean_reviews.append(process_review(review))
+    return clean_reviews
+
+def process_review(review):
+    without_html = remove_html_tags(review)
+    letters_only = remove_all_but_letters(without_html)
+    words = tokenize(letters_only)
+    return ' '.join(filter_stopwords(words, stopwords.words('english')))
+
+def remove_html_tags(text):
+    return BeautifulSoup(text, features='lxml').get_text()
+
+def remove_all_but_letters(text):
+    search_pattern = '[^a-zA-Z]'
+    replace_with = ' '
+    return re.sub(search_pattern, replace_with, text)
+
+def tokenize(text):
+    return text.lower().split()
+
+def filter_stopwords(words, stopwords):
+    result = []
+    for word in words:
+        if word in stopwords:
+            continue
+
+        result.append(word)
+    return result
